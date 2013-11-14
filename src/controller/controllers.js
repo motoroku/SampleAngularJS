d59bd8815c569ead2860fd84b970193028147b58
mori_yu <yuuto0609@gmail.com>
2013-11-14 20:50:33 +0900

    初回コミット
    angularjsのライブラリー追加したり、ちょっとindex.htmlとcontroller作ってみたり

---
+++ src/controller/controllers.js
@@ -0,0 +1,7 @@
+function SampleController($scope){
+    $scope.colors =[
+        {"name":"桜","color":"#fef4f4"},
+        {"name":"黄金","color":"#e6b422"},
+        {"name":"櫨染","color":"#d9a62e"}
+    ]
+}
