0697832939cb0e508a39132e2815ba014655a3e9
mori_yu <yuuto0609@gmail.com>
2013-11-14 22:35:33 +0900

    ルーティングで挫折
    なんかangularjsの方に問題があるのかな？

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -1,7 +1,22 @@
-function SampleController($scope){
-    $scope.colors =[
-        {"name":"桜","color":"#fef4f4"},
-        {"name":"黄金","color":"#e6b422"},
-        {"name":"櫨染","color":"#d9a62e"}
-    ]
+function ColorController($scope,$http){
+//    $scope.colors =[
+//        {"name":"桜色","kana":"さくらいろ","color":"#fef4f4"},
+//        {"name":"黄金","kana":"こがね","color":"#e6b422"},
+//        {"name":"櫨染","kana":"はじぞめ","color":"#d9a62e"}
+//    ]
+
+    $http.get("src/colors.json").success(function(data){
+        for(var i = 0; i<data.length;i++){data[i].like=0};
+       $scope.colors = data;
+    });
+};
+
+function ColorListItemController($scope){
+  $scope.increment=function(){
+      $scope.color.like++;
+  }
+};
+
+function ColorDetailController($scope,$routeParams){
+    $scope.colorId = $routeParams.colorId;
 };
