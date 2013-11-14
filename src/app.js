abc75beabe6bb5ffb021c4020a58745868a999ce
mori_yu <yuuto0609@gmail.com>
2013-11-14 22:35:33 +0900

    ルーティングで挫折
    なんかangularjsの方に問題があるのかな？

---
+++ src/app.js
@@ -0,0 +1,7 @@
+angular.module('colorset',["ngRoute"]).config(["$routeProvider", function($routeProvider){
+    $routeProvider
+        .when("/colors",{templateUrl:"../color-list.html", controller:ColorController})
+        console.log("hoge")
+        .when("/colors/:colorId",{templateUrl:"../color-detail.html",controller:ColorDetailController})
+        .otherwise({redirectTo:"/colors"});
+}]);
