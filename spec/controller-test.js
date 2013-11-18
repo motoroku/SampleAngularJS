f7795c8b2345d90c7ceb20c956d1077fb749aeb2
mori_yu <yuuto0609@gmail.com>
2013-11-19 00:13:42 +0900

    controllerの違う書き方でもテストできるか確認したら出来た
    serviceとの連携、$httpの利用はまだできてない。

--- spec/controller-test.js
+++ spec/controller-test.js
@@ -2,7 +2,9 @@ describe("sample", function() {
     var a
     var $injector;
     var myScope;
+    var myScope2;
     var ctrl;
+    var ctrl2;
 
     beforeEach(function() {
         a = 1;
@@ -12,7 +14,9 @@ describe("sample", function() {
     beforeEach(module("sampleApp"))
     beforeEach(inject(function($controller,$rootScope){
         myScope = $rootScope.$new();
+        myScope2 = $rootScope.$new();
         ctrl = $controller("ColorController",{$scope:myScope});
+        ctrl2 = $controller("ColorCtrl",{$scope:myScope2});
     }))
 
     it("green", function() {
@@ -21,6 +25,7 @@ describe("sample", function() {
         //demonstrates use of custom matcher
         expect(ctrl.tempMessage).toBe("hello");
         expect(myScope.colors[0].name).toBe("桜色");
+        expect(myScope2.colors[0].name).toBe("桜色");
     });
 
 //    it("red", function() {
