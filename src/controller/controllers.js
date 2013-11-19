2b5cdc05d97a8f9a51d3d399aec58e2eb8cf5331
Yuto Mori <mori_yu@mti.co.jp>
2013-11-19 18:39:59 +0900

    コントローラ間での$scopeの値受け渡しが出来た。
    angular.element,angular.expandを利用

--- src/controller/controllers.js
+++ src/controller/controllers.js
@@ -1,32 +1,34 @@
-var myapp = angular.module('sampleApp', []);
 
-myapp.controller('ColorController', function ($scope) {
-    $scope.colors = [
-        {"name": "桜色", "kana": "さくらいろ", "color": "#fef4f4"},
-        {"name": "黄金", "kana": "こがね", "color": "#e6b422"},
-        {"name": "櫨染", "kana": "はじぞめ", "color": "#d9a62e"}
-    ]
-    this.tempMessage = "hello";
-
-});
 function ColorCtrl($scope, $http) {
     $scope.colors = [
         {"name": "桜色", "kana": "さくらいろ", "color": "#fef4f4"},
         {"name": "黄金", "kana": "こがね", "color": "#e6b422"},
         {"name": "櫨染", "kana": "はじぞめ", "color": "#d9a62e"}
     ]
-//    $http.get("src/colors.json").success(function(data){
-//        for(var i = 0; i<data.length;i++){data[i].like=0};
-//       $scope.colors = data;
-//    });
+    $http.get("src/colors.json").success(function(data){
+        for(var i = 0; i<data.length;i++){data[i].like=0};
+       $scope.colors = data;
+    });
 };
 
-//function ColorListItemController($scope){
-//  $scope.increment=function(){
-//      $scope.color.like++;
-//  }
-//};
-//
-//function ColorDetailController($scope,$routeParams){
-//    $scope.colorId = $routeParams.colorId;
-//};
+function ColorListItemController($scope){
+  $scope.increment=function(){
+      $scope.color.like++;
+  }
+};
+
+function ColorDetailController($scope,$routeParams){
+    $scope.colorId = $routeParams.colorId;
+};
+
+function TempController($scope){
+    $scope.hoge = "hoge";
+    $scope.fuga = "fuga";
+    $scope.click = function(){
+        var parentScope = angular.element($('#color')).scope();
+        $scope.beforeColor = parentScope.colors[0];
+        $scope.editColor = angular.extend({},parentScope.colors[0]);
+
+    }
+
+}
